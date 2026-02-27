29739c1318b86229ec3d455ef141725ccc72a0ac
abhijeetpalanki <ajpalanki@gmail.com>
2026-02-26 18:02:12 -0600

    create recipes and categories pages with data

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -3,6 +3,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { neobrutalism } from "@clerk/themes";
 import Header from "../components/header";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         <body className={`${inter.variable} antialiased`}>
           <Header />
           <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
           <footer className="py-8 px-4 border-t">
             <div className="max-w-6xl mx-auto flex justify-center items-center">
               <p className="text-stone-500 text-sm">
